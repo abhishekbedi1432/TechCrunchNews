c73397475da2b764b355f2774f8812b263887677
Abhishek Bedi <macintosh@macintoshs-macbook-pro-2.local>
2018-11-25 21:45:45 +0530

    Initial Commit

---
+++ TechCrunchNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechCrunchNews
+//
+//  Created by macintosh on 25/11/18.
+//  Copyright © 2018 macintosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
